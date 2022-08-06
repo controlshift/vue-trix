4e567540dff21c1f1f1174083682986edf84baa8
rieg-ec <rieg@uc.cl>
2022-08-06 19:02:16 -0400

    fix(): comment emiteBeforeInitialize and migrate example app to vue3

--- src/index.js
+++ src/index.js
@@ -4,17 +4,16 @@
  * Github: https://github.com/hanhdt/vue-trix
  */
 
-import Vue from 'vue'
 import VueTrix from './components/VueTrix.vue'
-
-Vue.config.ignoredElements = ['trix-editor']
-Vue.component(VueTrix.name, VueTrix)
 
 const VueTrixPlugin = {
   install (app, options) {
     if (!options) {
       options = {};
     }
+    app.config.compilerOptions.isCustomElement = tag => tag === 'trix-editor'
+    app.config.ignoredElements = ['trix-editor']
+
     app.component('vue-trix', VueTrix);
   }
 };
